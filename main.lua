ef5914014fae81a5bd7575019fe8390eb16581b5
Soviet Kitsune <sovietkitsune@soviet.solutions>
2020-10-02 12:49:45 -0700

    Removed statistics server

--- main.lua
+++ main.lua
@@ -1,11 +1,6 @@
 local comrade = require 'Comrade'
 local fs = require 'fs'
 
-local net = require 'coro-net'
-local json = require 'json'
-
-local timer = require 'timer'
-
 local client, dotenv = comrade.Client, comrade.dotenv
 
 dotenv.config()
@@ -26,72 +21,14 @@ local bot = client(process.env.TOKEN, {
   gatewayFile = './cache/gateway.json'
 })
 
--- Update metrics --
-
-local function updateStats(write, bot)
-  write(json.encode {
-    method = 'set',
-    id = 'users',
-    name = 'Users',
-    auth = process.env.METRICS_AUTH,
-    value = #bot.users
-  })
-
-  write(json.encode {
-    method = 'set',
-    id = 'guilds',
-    name = 'Guilds',
-    auth = process.env.METRICS_AUTH,
-    value = #bot.guilds
-  })
-
-  write(json.encode {
-    method = 'set',
-    id = 'dms',
-    name = 'Private channels',
-    auth = process.env.METRICS_AUTH,
-    value = #bot.privateChannels
-  })
-end
-
 require './watcher'(bot)
-
-local function write()
-  -- To prevent errors if not loaded
-end
 
 bot:on('ready', function()
   bot:addCommand(comrade.Status)
 
   logger.info 'Bot is online; This usually means it was restarted'
-
-  -- Connect to metrics --
-  local port = tonumber(process.argv[2])
-
-  if port then
-    local succ, realWrite = net.connect({
-      port = port
-    })
-
-    if not succ then
-      logger.error 'Unable to connect to metrics; exiting...'
-      process:exit(1)
-    end
-
-    write = function(str)
-      realWrite(str .. '|')
-    end
-
-    updateStats(write, bot)
-  end
 end)
 
 bot:on('error', logger.error)
-
--- Update metrics every minute --
-
-timer.setInterval(60000, function()
-  coroutine.wrap(updateStats)(write, bot)
-end)
 
 bot:login()
