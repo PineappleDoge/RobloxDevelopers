23d5e114f7b855b65261d13478c804155a430ebd
Soviet Kitsune <sovietkitsune@soviet.solutions>
2020-10-07 12:26:25 -0700

    Possible auto unmute for auto-mod

--- events/messageCreate.event.lua
+++ events/messageCreate.event.lua
@@ -13,20 +13,26 @@ local channels = showcase.channels
 
 local msgs = {}
 
-timer.setInterval(util.minutes(0.5), function()
+timer.setInterval(util.seconds(15), function()
   msgs = {}
 end)
 
 return lua.class('messageCreate', {
   execute = function(msg)
-    if msg.embed and table.search(channels, msg.channel.id) then
+    if (msg.embed or msg.attachment) and table.search(channels, msg.channel.id) then
       msg:addReaction '⭐'
     end
 
     msgs[msg.author.id] = (msgs[msg.author.id] and msgs[msg.author.id] + 1) or 1
 
-    if msgs[msg.author.id]  > 6 then
---       msg.member:addRole(config.muted)
+    if msgs[msg.author.id] > 6 then
+      msg:reply(msg.author.tag .. ' has been muted for spam')
+      msg.member:addRole(config.muted)
+
+      timer.setTimeout(util.minutes(30), coroutine.wrap(function()
+        msgs[msg.author.id] = 0
+        msg.member:removeRole(config.muted)
+      end))
     end
   end}, event, function(self)
   self:super(self,'new')
